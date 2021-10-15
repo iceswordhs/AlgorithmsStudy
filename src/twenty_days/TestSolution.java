4bc898a2454807a06ff00a4724f2e20ad6447871
iceswordhs <75706056+iceswordhs@users.noreply.github.com>
2021-10-15 21:38:03 +0800

    10.15

---
+++ src/twenty_days/TestSolution.java
@@ -0,0 +1,18 @@
+package twenty_days;
+
+import twenty_days.day8.feibonaqishulie_jianzhioffer10_I;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Hs
+ * @Date 2021/10/14 17:35
+ */
+public class TestSolution {
+    public static void main(String[] args) {
+        feibonaqishulie_jianzhioffer10_I feibonaqishulie_jianzhioffer10_i = new feibonaqishulie_jianzhioffer10_I();
+        System.out.println(feibonaqishulie_jianzhioffer10_i.fib(2));
+
+    }
+}
