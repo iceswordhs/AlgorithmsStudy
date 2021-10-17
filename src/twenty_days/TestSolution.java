3b35eaaabefc6af2a2c502bea00289be5571653c
iceswordhs <75706056+iceswordhs@users.noreply.github.com>
2021-10-17 19:22:15 +0800

    10.17

--- src/twenty_days/TestSolution.java
+++ src/twenty_days/TestSolution.java
@@ -1,6 +1,6 @@
 package twenty_days;
 
-import twenty_days.day8.feibonaqishulie_jianzhioffer10_I;
+import twenty_days.day16.diandaoerjinziwei190;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,14 +11,11 @@ import java.util.List;
  */
 public class TestSolution {
     public static void main(String[] args) {
+        diandaoerjinziwei190 solution = new diandaoerjinziwei190();
+        System.out.println(solution.reverseBits(-3));
+        //System.out.println(Integer.toBinaryString(5));
+        //System.out.println(Integer.toBinaryString(-6));
 
-    }
-    public int get(int n){
-        int ans=0;
-        while(n!=0){
-            ans+=n%10;
-            ans=ans/10;
-        }
-        return ans;
+
     }
 }
