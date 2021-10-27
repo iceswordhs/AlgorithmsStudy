b3d7533e5a9947bb0e9d5baebbc7b18ca5159455
iceswordhs <75706056+iceswordhs@users.noreply.github.com>
2021-10-27 21:53:36 +0800

    10.27

---
+++ src/swordoffer1/solution43.java
@@ -0,0 +1,30 @@
+package swordoffer1;
+
+/**
+ * @author Hs
+ * @Date 2021/10/27 20:21
+ */
+
+/**
+ * 输入一个整数 n ，求1～n这n个整数的十进制表示中1出现的次数。
+ *
+ * 例如，输入12，1～12这些整数中包含1 的数字有1、10、11和12，1一共出现了5次。
+ *
+ * 思路：
+ */
+public class solution43 {
+    public int countDigitOne(int n) {
+        int digit = 1, res = 0;
+        int high = n / 10, cur = n % 10, low = 0;
+        while(high != 0 || cur != 0) {
+            if(cur == 0) res += high * digit;
+            else if(cur == 1) res += high * digit + low + 1;
+            else res += (high + 1) * digit;
+            low += cur * digit;
+            cur = high % 10;
+            high /= 10;
+            digit *= 10;
+        }
+        return res;
+    }
+}
