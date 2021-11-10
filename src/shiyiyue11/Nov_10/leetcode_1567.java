789d0cc36686d9765185419754e4f8a662b0dfe5
iceswordhs <75706056+iceswordhs@users.noreply.github.com>
2021-11-10 22:26:57 +0800

    11.10

---
+++ src/shiyiyue11/Nov_10/leetcode_1567.java
@@ -0,0 +1,40 @@
+package shiyiyue11.Nov_10;
+
+/**
+ * @author Hs
+ * @Date 2021/11/10 22:12
+ */
+
+/**
+ * 给你一个整数数组 nums ，请你求出乘积为正数的最长子数组的长度。
+ *
+ * 一个数组的子数组是由原数组中零个或者更多个连续数字组成的数组。
+ *
+ * 请你返回乘积为正数的最长子数组长度。
+ *
+ * 思路：
+ */
+public class leetcode_1567 {
+    public int getMaxLen(int[] nums) {
+        int length = nums.length;
+        int positive = nums[0] > 0 ? 1 : 0;
+        int negative = nums[0] < 0 ? 1 : 0;
+        int maxLength = positive;
+        for (int i = 1; i < length; i++) {
+            if (nums[i] > 0) {
+                positive++;
+                negative = negative > 0 ? negative + 1 : 0;
+            } else if (nums[i] < 0) {
+                int newPositive = negative > 0 ? negative + 1 : 0;
+                int newNegative = positive + 1;
+                positive = newPositive;
+                negative = newNegative;
+            } else {
+                positive = 0;
+                negative = 0;
+            }
+            maxLength = Math.max(maxLength, positive);
+        }
+        return maxLength;
+    }
+}
