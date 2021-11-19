3d0e2ba2ea648a69f6ece37278484a339eb004c6
iceswordhs <75706056+iceswordhs@users.noreply.github.com>
2021-11-19 22:10:55 +0800

    11.19

--- src/shiyiyue11/TestMianshi.java
+++ src/shiyiyue11/TestMianshi.java
@@ -3,6 +3,7 @@ package shiyiyue11;
 import shiyiyue11.Nov_16.leetcode_165;
 import shiyiyue11.Nov_17.leetcode_240;
 import shiyiyue11.Nov_17.leetcode_931;
+import shiyiyue11.Nov_19.leetcode_397;
 
 import java.util.Arrays;
 
@@ -12,7 +13,7 @@ import java.util.Arrays;
  */
 public class TestMianshi {
     public static void main(String[] args) {
-        leetcode_240 solution = new leetcode_240();
-        solution.searchMatrix(new int[][]{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22}, {10,13,14,17,24},{18,21,23,26,30}},20);
+        leetcode_397 solution = new leetcode_397();
+        solution.integerReplacement(65535);
     }
 }
