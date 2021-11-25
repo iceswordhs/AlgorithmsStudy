a29de31890ac2ec8b1b32dd11d90d75c0c8ba94a
iceswordhs <75706056+iceswordhs@users.noreply.github.com>
2021-11-25 21:23:05 +0800

    11.25

--- src/shiyiyue11/TestMianshi.java
+++ src/shiyiyue11/TestMianshi.java
@@ -5,6 +5,7 @@ import shiyiyue11.Nov_17.leetcode_240;
 import shiyiyue11.Nov_17.leetcode_931;
 import shiyiyue11.Nov_19.leetcode_397;
 import shiyiyue11.Nov_21.leetcode_516;
+import shiyiyue11.Nov_25.leetcode_377;
 
 import java.util.Arrays;
 
@@ -14,7 +15,7 @@ import java.util.Arrays;
  */
 public class TestMianshi {
     public static void main(String[] args) {
-        leetcode_516 solution = new leetcode_516();
-        solution.longestPalindromeSubseq("cbbd");
+        leetcode_377 solution = new leetcode_377();
+        solution.combinationSum4(new int[]{1,2,3},4);
     }
 }
