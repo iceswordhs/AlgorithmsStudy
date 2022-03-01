3087b6d42e2a967c6b827b6763cd5235705e29a0
iceswordhs <75706056+iceswordhs@users.noreply.github.com>
2022-03-01 11:10:22 +0800

    3.1

--- src/shiyiyue11/Nov_26/leetcode_219.java
+++ src/shiyiyue11/Nov_26/leetcode_219.java
@@ -1,6 +1,7 @@
 package shiyiyue11.Nov_26;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * @author Hs
@@ -12,18 +13,32 @@ import java.util.HashMap;
  * 判断数组中是否存在两个不同的索引i和j，
  * 使得nums [i] = nums [j]，并且 i 和 j的差的 绝对值 至多为 k。
  *
- * 思路：并查集
+ * 思路1：并查集
+ * 思路2：滑动窗口
  */
 public class leetcode_219 {
+    //public boolean containsNearbyDuplicate(int[] nums, int k) {
+    //    HashMap<Integer, Integer> temp = new HashMap<>();
+    //    for(int i = 0; i < nums.length; i++) {
+    //        if (temp.containsKey(nums[i])) {
+    //            if (i - temp.get(nums[i]) <= k) {
+    //                return true;
+    //            }
+    //        }
+    //        temp.put(nums[i], i);
+    //    }
+    //    return false;
+    //}
+
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> temp = new HashMap<>();
-        for(int i = 0; i < nums.length; i++) {
-            if (temp.containsKey(nums[i])) {
-                if (i - temp.get(nums[i]) <= k) {
-                    return true;
-                }
+        HashSet<Integer> set = new HashSet<>();
+        for (int i=0 ;i< nums.length;i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            // 窗口后移，移除前面的元素
+            if(set.size()>k){
+                set.remove(nums[i-k]);
             }
-            temp.put(nums[i], i);
         }
         return false;
     }
